164991f78772300b618bfca4344a15d8dd6eb84f
WohooWahoo <wohoowahoo@gmail.com>
2023-09-23 11:50:07 -0500

    added Calculate distance button and label

--- nVisti_codeTest/nVisti_codeTest/ContentView.swift
+++ nVisti_codeTest/nVisti_codeTest/ContentView.swift
@@ -39,9 +39,46 @@ struct ContentView: View {
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
                 .padding()
+                
+                Button(action: {
+                    calculateDistance()
+                }) {
+                    Text("Calculate Distance")
+                        .font(.headline)
+                        .padding()
+                        .background(Color.purple)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(50)
+                
+                Text("Distance: \(distance) km")
+                    .padding(50)
             }
         }
 
+    }
+    
+    func calculateDistance() {
+        guard let originLat = Double(originLatitude),
+              let originLon = Double(originLongitude),
+              let destinationLat = Double(destLatitude),
+              let destinationLon = Double(destLongitude)
+        else {
+            // Handle invalid input here if necessary
+            return
+        }
+        
+        let originCoordinate = CLLocationCoordinate2D(latitude: originLat, longitude: originLon)
+        let destinationCoordinate = CLLocationCoordinate2D(latitude: destinationLat, longitude: destinationLon)
+        
+        let originLocation = CLLocation(latitude: originCoordinate.latitude, longitude: originCoordinate.longitude)
+        let destinationLocation = CLLocation(latitude: destinationCoordinate.latitude, longitude: destinationCoordinate.longitude)
+        
+        let distanceInMeters = originLocation.distance(from: destinationLocation)
+        let distanceInKilometers = distanceInMeters / 1000
+        
+        distance = String(format: "%.2f", distanceInKilometers)
     }
 }
 
